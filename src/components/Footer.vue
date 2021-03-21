a9f084958d1774793c3420a6ecabb60c382d3cb2
Krenare Rafuna <kr43889@ubt-uni.net>
2021-03-21 17:11:28 +0100

    footer- update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,7 +13,7 @@
             <div class="column-4">
               <div class="footer_location">
                 <a href="#">
-                  <!-- <img v-bind:src="content/images/pin.svg" alt=""> -->
+                  <img v-bind:src="'content/images/pin.svg'" alt="">
                   <h3>Street Name, 123 City</h3>
                 </a>
               </div>
@@ -21,7 +21,7 @@
             <div class="column-4">
               <div class="footer_email">
                 <a href="#">
-                  <!-- <img v-bind:src="content/images/mail.svg" alt=""> -->
+                  <img v-bind:src="'content/images/mail.svg'" alt="">
                   <h3>example@mail.com</h3>
                 </a>
               </div>
@@ -29,25 +29,28 @@
             <div class="column-4">
               <div class="footer_phoneNumber">
                 <a href="#">
-                  <!-- <img v-bind:src="content/images/phone.svg" alt=""> -->
+                  <img v-bind:src="'content/images/phone.svg'" alt="">
                   <h3>00 111 222 333</h3>
                 </a>
               </div>
             </div>
           </div>
-          <!-- <div class="footer_socials block_section">
+           <div class="footer_socials block_section">
                             <a href="https://www.facebook.com/">
-                                <img v-bind:src="content/images/logo.svg" alt="">
+                                <img v-bind:src="'content/images/logo.svg'" alt="">
                             </a>
                             <a href="https://www.instagram.com/">
-                                <img v-bind:src="content/images/instagram.svg" alt="">
+                                <img v-bind:src="'content/images/instagram.svg'" alt="">
                             </a>
                             <a href="https://twitter.com">
-                                <img v-bind:src="content/images/twitter.svg" alt="">
+                                <img v-bind:src="'content/images/twitter.svg'" alt="">
                             </a>
-                        </div> -->
+                        </div> 
           <div class="footer_contact block_section">
-            <a href="contact.html">CONTACT</a>
+            <!-- <a href="contact.html">CONTACT</a> -->
+             <router-link :to="'/contact/'">
+                        CONTACT
+              </router-link>
           </div>
           <div class="footer_copyright block_section">
             <p>Vanoa - © 2020</p>
